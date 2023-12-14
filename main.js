15dbb394bc11ca0bb5d3b15ae6524d921ec15f29
Akramatti <129142486+Akramatti@users.noreply.github.com>
2023-12-14 13:50:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,152 @@
+// =====================================USERS_STORAGE===================================================================
+
+window.users = {
+    SYMBOS: {password: "HalloWelt", lessons: {cs: [], math: []}, name: 'Ihor'},
+    ATTOS: {password: "CiaoMondo", lessons: {cs: [], math: []}, name: 'Akram'},
+    admin: {password: "admin", lessons: {cs: [], math: []}, name: 'admin'},
+}
+
+
+// =====================================IMPORT/EXPORT===================================================================
+
+function importUsers() {
+    // importing the users form localStorage
+    if (localStorage.getItem("users")) {
+        window.users = JSON.parse(localStorage.getItem("users"))
+    }
+}
+
+function exportUsers() {
+    // exporting the users form localStorage
+    localStorage.setItem("users", JSON.stringify(window.users))
+    return JSON.parse(localStorage.getItem("users"))
+}
+
+
+// =====================================AUTHENTICATION==================================================================
+
+function getCurrentUser() {
+    const cookieKeys = Object.keys(Cookies.get())
+
+    // If cookie array is not empty => return the name of authenticated user => else nothing
+    if (cookieKeys.length) {
+        return cookieKeys[0].toString()
+    }
+
+    return null
+}
+
+function authentication() {
+    const username = $('#username').val();
+    const password = $('#password').val();
+
+    if (username && username in window.users && password === window.users[username].password) {
+        Cookies.set(username, window.users[username].name)
+        window.location.href = 'user.html';
+        return
+    }
+
+    alert("Credential invalid!");
+}
+
+function logout() {
+    // Remove cookies of authorized user and replace to login page
+    Cookies.remove(getCurrentUser())
+    window.location.href = 'login.html'
+}
+
+
+// =====================================GRADES_AVERAGE_FUNCTIONS========================================================
+
+function getAverageGradesOfLesson(lesson) {
+    // The function return the medium of all grades of one lesson
+    const user = getCurrentUser()
+    const arrayOfLessonsGrades = window.users[user].lessons[lesson]
+
+    if (user && arrayOfLessonsGrades.length) {
+        const sum = arrayOfLessonsGrades.reduce((v, i) => (v + i))
+        return sum / arrayOfLessonsGrades.length
+    }
+
+    return 0
+}
+
+function getAverageGradesTotal() {
+    // The function return the average of all lessons of the one user
+    const averageCS = getAverageGradesOfLesson('cs')  // Get the average grades of lesson CS = (sum/count)
+    const averageMath = getAverageGradesOfLesson('math')  // Get the average grades of lesson Math = (sum/count)
+    return (averageMath + averageCS) / 2
+}
+
+
+// =====================================DISPLAYING======================================================================
+
+
+function getArrayGratesFormatted(lesson) {
+    // The function formatted array to humanity readable string
+    const arrayOfLessonsGrades = window.users[getCurrentUser()].lessons[lesson];  // Get raw array [1, 5, 2, 10, 9]
+    return `<p> ${arrayOfLessonsGrades.join(', ')}</p>` // Formatting in humanity readable string => "1, 5, 2, 10, 9"
+}
+
+function printVotes(lesson) {
+    // The function update and display only the average of lesson and their array
+    $(`#${lesson}Chronology`).html(getArrayGratesFormatted(lesson))  // The list of grades of one lesson
+    $(`#${lesson}Value`).html(`${lesson}<br>avr grade: ${getAverageGradesOfLesson(lesson)}`)  // The medium value
+}
+
+function displayAllGrades() {
+    // The function update and display the new values in html. do the calculate of avg for each lesson and total avg
+    printVotes('cs')  // Update grades for CS
+    printVotes('math')  // Update grades for Math
+    $("#averageGrades").html(`Total average: ${getAverageGradesTotal()}`) // Update total grades for each lesson
+}
+
+
+// =====================================MANAGEMENT_OF_GRADES============================================================
+
+function addNewGrade(lesson) {
+    // The closure function return the object functionInner and this function add the new value(grade) into array
+    const user = getCurrentUser()
+
+    function addNewGradeInner() {
+        const grade = parseInt($(`#${lesson}`).val())  // Getting value from input form
+        if (grade) {  // Check if grade != 0 or null or undefined
+            window.users[user].lessons[lesson].push(grade)  // Add a grade to the list of the lesson of our user
+            displayAllGrades()  // Displaying new updates
+            exportUsers()  // Export new updates to localStorage
+        }
+    }
+
+    return addNewGradeInner
+}
+
+function removeAllGrades() {
+    window.users[getCurrentUser()].lessons = {cs: [], math: []}  // Clean her last grades of lessons
+    displayAllGrades()  // displaying new updates
+    exportUsers()  // Export new updates to localStorage
+}
+
+
+// =====================================DOCUMENT_IS_READY===============================================================
+
+$(document).ready(() => {
+
+    // import/export
+    importUsers()
+    exportUsers()
+
+    // Auth
+    $('#signIn').click(authentication);
+    $('#logout').click(logout);
+
+    $('#delete').click(removeAllGrades);
+    $('#saveMath').click(addNewGrade('math'));
+    $('#saveInformatics').click(addNewGrade('cs'));
+
+    // Checking if user is already authorized
+    if (getCurrentUser()) {
+        displayAllGrades()
+    }
+
+});
+
